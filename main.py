eb5820620be7bebdb259db8d0d8f912969418e4a
Socrates <dubstepero10@gmail.com>
2020-10-10 20:59:37 -0600

    starting with python

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+print("Hello World")
+
+
+def greet(name):
+    print("Hello my name is", name)
+
+
+greet("Socrates")
+
+
+def squareNum(a):
+    # num = input("Enter a number")
+    print(math.pow(num, 2))
+
+# squareNum(3)
+
+
+for num in range(2, 9):
+    print(squareNum(num))
